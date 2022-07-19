2729d77a49e60cf331f30ba3447ea24c1d7ef3d9
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2022-07-19 11:22:39 +0530

    4thcommit

--- macy.py
+++ macy.py
@@ -5,7 +5,7 @@ def Repeat(x):
 	_size = len(x)
 	repeated = []
 	for i in range(_size):
-		k = i + 3
+		k = i + 4
 		for j in range(k, _size):
 			if x[i] == x[j] and x[i] not in repeated:
 				repeated.append(x[i])
